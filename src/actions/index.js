17d12c347af061f6cafdceff9903e08366455db2
Maria <jn-01@centurytel.net>
2019-09-26 11:27:46 -0500

    Add export functions

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const increment = () => ({
+  type: 'INCREMENT'
+});
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+});
+
+export const input = (number) => ({
+  type: 'INPUT',
+  number
+});
+
+export const inputNumber = () => ({
+  type: 'INPUTNUMBER'
+});
